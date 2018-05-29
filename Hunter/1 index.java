3622bcd07ea47ef990610eb30ce0738d6e546c8c
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2018-05-29 21:09:53 +0530

    Create 1 index.java

---
+++ Hunter/1 index.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ind{
+    public static void main(String a[]){
+        Scanner s=new Scanner(System.in);
+        String jl=s.next();
+        String k=s.next();
+        int y=jl.indexOf(k);
+        System.out.println(y);
+    }
+}
