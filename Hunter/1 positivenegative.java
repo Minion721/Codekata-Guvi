685fe8d0f14ff9b409f17ddc445accd982a345f5
Nivetha Manikandan <34644928+NivethaM19@users.noreply.github.com>
2018-05-29 20:51:17 +0530

    Update 1 positivenegative.java

--- Hunter/1 positivenegative.java
+++ Hunter/1 positivenegative.java
@@ -2,17 +2,17 @@ import java.util.*;
 import java.lang.*;
 public class positivenegative{
      public static void main(String aa[]){
-     int n;
+     int nm;
      Scanner s=new Scanner(System.in);
-     n=s.nextInt();
-     int a[]=new int[n];
-     for(int i=0;i<n;i++){
+     nm=s.nextInt();
+     int a[]=new int[nm];
+     for(int i=0;i<nm;i++){
          a[i]=s.nextInt();
 
      }
      LinkedHashSet<Integer>b=new LinkedHashSet<Integer>();
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=0;j<nm;j++){
             if(a[i]+a[j]==0){
              b.add(a[i]);
              b.add(a[j]);
